096da68e0bb91691c44a5f3aee27221910b3c5a8
Vlad Stoian <vlad.stoian@avrios.com>
2024-10-15 15:26:02 +0200

    lala 6

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "B0rk3d"
 
     def betRequest(self, game_state):
         return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state[
