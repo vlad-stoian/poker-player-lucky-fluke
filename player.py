1927fc4e870e181a4d933455be687ea9a18ddfaa
Vlad Stoian <vlad.stoian@avrios.com>
2024-10-15 14:34:18 +0200

    lala 4

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
 
     def betRequest(self, game_state):
         return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state[
-            "minimum_raise"]
+            "minimum_raise"] + 1
 
     def showdown(self, game_state):
         pass
