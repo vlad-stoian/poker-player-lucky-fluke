5ea623c25ddcd03ca2715b1557b420a11cdb31c0
Vlad Stoian <vlad.stoian@avrios.com>
2024-10-15 14:33:39 +0200

    lala 3

--- player.py
+++ player.py
@@ -2,8 +2,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-
-        return max(p["bet"] for p in game_state["players"]) + 1
+        return game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + game_state[
+            "minimum_raise"]
 
     def showdown(self, game_state):
         pass
